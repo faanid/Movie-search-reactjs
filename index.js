98d88788cfc013b829703e87434fd3db8151f3e8
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-28 11:21:59 +0330

    "create the search Movies function"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SearchMovies from "./searchmovies";
+import SearchMovies from "./searchMovies";
 
 class Main extends React.Component {
   render() {
--- searchmovies.js
+++ searchmovies.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api
+  _key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=
+  ${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json;
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -12,7 +31,7 @@ export default function SearchMovies() {
         name="query"
         placeholder="i.e. Jurassic Park"
       />
-      <button class="button" type="submit">
+      <button className="button" type="submit">
         Search
       </button>
     </form>
