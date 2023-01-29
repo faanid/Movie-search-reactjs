8b49504aa01d9818d69a2968ec3a0b01facbedda
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-29 16:01:36 +0330

    Display Movie Information

--- searchmovies.js
+++ searchmovies.js
@@ -26,20 +26,46 @@ export default function SearchMovies() {
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park"
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movie.id}>
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + " poster"}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
