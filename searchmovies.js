db42b1f67baf73ce718ac9b51e6de74e06ba537f
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-01-31 20:09:35 +0330

    create the movie card component

--- searchmovies.js
+++ searchmovies.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import MovieCard from "./movieCard";
 
-export default function SearchMovies() {
+export default function SearchMovies(props) {
   const [query, setQuery] = useState("");
 
   const [movies, setMovies] = useState([]);
@@ -47,23 +48,7 @@ export default function SearchMovies() {
         {movies
           .filter((movie) => movie.poster_path)
           .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} key={movie.id} />
           ))}
       </div>
     </>
